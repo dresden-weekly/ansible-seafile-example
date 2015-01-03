731a9ae59b2aee6483d7aa47921f8f28a9838555
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2015-01-03 01:32:05 +0100

    switched to vagrant-ansible-remote usage

--- remote.bat
+++ remote.bat
@@ -1,43 +1,11 @@
 @echo off
+:: This script should be placed in the root of your project
 SETLOCAL ENABLEEXTENSIONS
 set OLD_CD=%CD%
 cd /D "%~dp0"
 
-set PROVISION_TARGET=
-if [%1]==[] goto :aftertarget
-if EXIST "ansible\hosts\%1" (
-  set PROVISION_TARGET=%1
-  echo Target: %1
-  shift
-)
-:aftertarget
-
-set PROVISION_ACTION=
-if [%1]==[] goto :afteraction
-if EXIST "ansible\%1.yml" (
-  set PROVISION_ACTION=%1
-  echo Action: %1
-  shift
-)
-:afteraction
-
-if [%1]==[] goto :no_args
-set PROVISION_ARGS=%1
-shift
-:loopargs
-if [%1]==[] goto :afterargs
-set PROVISION_ARGS=%PROVISION_ARGS% %1
-shift
-goto :loopargs
-:afterargs
-set PROVISION_ARGS=%PROVISION_ARGS:\=/%
-echo Arguments: %PROVISION_ARGS%
-:no_args
-
-:: windows has no local ansible
-set VM_ANSIBLE=1
-echo vagrant provision
-vagrant provision
+set VAGRANT_ANSIBLE_REMOTE=vagrant-ansible-remote
+call "%VAGRANT_ANSIBLE_REMOTE%/vagrant/ssh-ansible.bat" %*
 
 cd /D "%OLD_CD%"
 ENDLOCAL
--- remote.sh
+++ remote.sh
@@ -1,43 +1,13 @@
 #!/bin/sh
-if [ ! -z "$1" ] ; then
-  if [ -s "ansible/groups/$1.yml" ]; then
-    export PROVISION_GROUPS=$1
-    PROVISION_VAGRANT=true
-    echo Groups: $1
-    shift
-  elif [ -s "ansible/hosts/$1" ] ; then
-    export PROVISION_TARGET=$1
-    echo Target: $1
-    shift
-  fi
-fi
 
-if [ ! -z "$1" ] ; then
-  if [ -s "ansible/$1.yml" ] ; then
-    export PROVISION_ACTION=$1
-    echo Action: $1
-    shift
-  fi
-fi
+VAGRANT_ANSIBLE_REMOTE=vagrant-ansible-remote
 
-if [ ! -z "$1" ] ; then
-  export PROVISION_ARGS=$@
-  echo Arguments: $@
-fi
+ANSIBLE_BASE_FOLDER=${ANSIBLE_BASE_FOLDER:=$BASE_FOLDER/ansible}
 
-export ANSIBLE_DIR=${ANSIBLE_DIR:=`pwd`/.ansible}
-if [ "true" = "$PROVISION_VAGRANT" -o "vagrant" = "${PROVISION_TARGET:=vagrant}" ] ; then
-  mkdir -p $ANSIBLE_DIR
-  PATH=$ANSIBLE_DIR:$PATH
-  export PATH
-  echo vagrant provision
-  exec vagrant provision
+VAGRANT_OPTIONAL=true source ${VAGRANT_ANSIBLE_REMOTE}/ansible/run-args.sh
+
+if [ "$ANSIBLE_RUN_LOCAL" = true ]; then
+  source ${VAGRANT_ANSIBLE_REMOTE}/ansible/run.sh
 else
-  echo running ansible
-  # export ANSIBLE_HOST_KEY_CHECKING=False
-  # export PYTHONUNBUFFERED=1
-  # export ANSIBLE_FORCE_COLOR=1
-  export BASE_FOLDER=${BASE_FOLDER:=`pwd`/ansible}
-  export RUN_ANSIBLE=true
-  exec $BASE_FOLDER/run.sh $PROVISION_TARGET ${PROVISION_ACTION:=provision}
+  source ${VAGRANT_ANSIBLE_REMOTE}/vagrant/ssh-ansible.sh
 fi
