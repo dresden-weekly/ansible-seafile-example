0c03c1402dfe19d0801c295d67be2989f2912b5d
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2015-01-03 04:49:05 +0100

    fixed linux/mac scripts

--- remote.sh
+++ remote.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 VAGRANT_ANSIBLE_REMOTE=vagrant-ansible-remote
-
+BASE_FOLDER=${BASE_FOLDER:=`pwd`}
 ANSIBLE_BASE_FOLDER=${ANSIBLE_BASE_FOLDER:=$BASE_FOLDER/ansible}
 
-VAGRANT_OPTIONAL=true source ${VAGRANT_ANSIBLE_REMOTE}/ansible/run-args.sh
+VAGRANT_OPTIONAL=true
+source ${VAGRANT_ANSIBLE_REMOTE}/ansible/run-args.sh
 
 if [ "$ANSIBLE_RUN_LOCAL" = true ]; then
   source ${VAGRANT_ANSIBLE_REMOTE}/ansible/run.sh
